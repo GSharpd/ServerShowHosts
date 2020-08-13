3e11012a85aba539fc9ee37792ae5cb631552ca2
Gustavo Vieira <gmtnvieira@gmail.com>
2020-08-13 19:24:23 -0300

    initial push

---
+++ ServerShowHosts/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+
+namespace ServerShowHosts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string path = @"C:/Windows/System32/drivers/etc/hosts";
+
+            string readText = "";
+
+            using (StreamReader file = new StreamReader(path))
+            {
+               readText = file.ReadToEnd();
+            }
+
+            return View(readText);
+        }
+    }
+}
